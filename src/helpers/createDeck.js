4e475c1d979209df39c150c187c5cf0724465b60
Thomas Ghaye <thomasghaye@hotmail.com>
2021-04-13 15:40:42 +0200

    Updated image index

--- src/helpers/createDeck.js
+++ src/helpers/createDeck.js
@@ -1,17 +1,17 @@
 export default function createDeck() {
   const cardsData = [
-    { id: 0, name: 'Le Parthénon', date: -447 },
-    { id: 1, name: 'Notre-Dame de Paris', date: 1162 },
-    { id: 2, name: 'Le mont Saint-Michel', date: 708 },
-    { id: 3, name: 'La pyramide de Khéops', date: -2560 },
-    { id: 4, name: 'Le Colisée', date: 70 },
-    { id: 5, name: 'La tour Eiffel', date: 1887 },
-    { id: 6, name: 'La tour de Pise', date: 1172 },
-    { id: 7, name: 'Le Palais idéal', date: 1879 },
-    { id: 8, name: 'La statue de la Liberté', date: 1886 },
-    { id: 9, name: 'Le Taj Mahal', date: 1621 },
-    { id: 10, name: 'Le cercle de Stonehenge', date: -2800 },
-    { id: 11, name: 'Les alignements de Carnac', date: -5000 }
+    { id: 1, name: 'Le Parthénon', date: -447 },
+    { id: 2, name: 'Notre-Dame de Paris', date: 1162 },
+    { id: 3, name: 'Le mont Saint-Michel', date: 708 },
+    { id: 4, name: 'La pyramide de Khéops', date: -2560 },
+    { id: 5, name: 'Le Colisée', date: 70 },
+    { id: 6, name: 'La tour Eiffel', date: 1887 },
+    { id: 7, name: 'La tour de Pise', date: 1172 },
+    { id: 8, name: 'Le Palais idéal', date: 1879 },
+    { id: 9, name: 'La statue de la Liberté', date: 1886 },
+    { id: 10, name: 'Le Taj Mahal', date: 1621 },
+    { id: 11, name: 'Le cercle de Stonehenge', date: -2800 },
+    { id: 12, name: 'Les alignements de Carnac', date: -5000 }
   ];
 
   return Phaser.Math.RND.shuffle(cardsData);
