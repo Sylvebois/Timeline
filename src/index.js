338a638fd14dd3c581c6a0cdab9ee25425691ef3
Thomas Ghaye <thomasghaye@hotmail.com>
2021-04-21 11:52:22 +0200

    Added scaling into config

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const config = {
   height: 700,
   backgroundColor: 0x3f3f3f,
   title: 'Timeline',
+  scale : {
+    mode : Phaser.Scale.FIT,
+    autoCenter : Phaser.Scale.CENTER_BOTH,
+  },
   scene: [Game],
 };
 
