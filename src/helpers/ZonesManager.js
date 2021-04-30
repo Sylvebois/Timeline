b6b32f7095a9c8ea633ce0a3fe40c24de8cc3e56
Sylvebois <thomasghaye@hotmail.com>
2021-04-30 14:36:20 +0200

    Returns the container outline

--- src/helpers/ZonesManager.js
+++ src/helpers/ZonesManager.js
@@ -21,11 +21,13 @@ export default class ZonesManager {
     let dropZoneOutline = this.scene.add.graphics();
     dropZoneOutline.lineStyle(4, 0xff69b4);
     dropZoneOutline.strokeRect(dropZone.x - dropZone.input.hitArea.width / 2, dropZone.y - dropZone.input.hitArea.height / 2, dropZone.input.hitArea.width, dropZone.input.hitArea.height);
+    return dropZoneOutline;
   }
 
   renderContainer(container) {
     let containerOutline = this.scene.add.graphics();
     containerOutline.lineStyle(4, 0xff69b4);
     containerOutline.strokeRect(container.x, container.y, container.width, container.height);
+    return containerOutline;
   }
 }
