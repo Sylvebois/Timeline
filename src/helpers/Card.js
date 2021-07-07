3dbc2f838fdfaf2cabed300bb02774518f7842a5
Sylvebois <thomasghaye@hotmail.com>
2021-07-07 11:42:10 +0200

    Updated and reordered images

--- src/helpers/Card.js
+++ src/helpers/Card.js
@@ -32,52 +32,29 @@ export class Card extends Phaser.GameObjects.Container {
 }
 
 export const cardsData = [
-  { id: 0, name: 'Début de la construction de Notre-Dame de Paris', creator: '', collection: 'France', date: 1163 },
-  { id: 1, name: 'Premier recueil des Fables de La Fontaine', creator: '', collection: 'France', date: 1668 },
-  { id: 2, name: 'Siège de Gergovie et d’Alésia', creator: '', collection: 'France', date: -52 },
-  { id: 3, name: 'Attila, le roi des Huns, envahit la Gaule', creator: '', collection: 'France', date: 451 },
-  { id: 4, name: 'Début du règne de Charlemagne', creator: '', collection: 'France', date: 768 },
-  { id: 5, name: 'Première apparition de Sherlock Holmes', creator: 'Arthur Conan Doyle', collection: 'General', date: 1887 },
-  { id: 6, name: 'Sortie du film La Guerre des Etoiles', creator: 'Georges Lucas', collection: 'General', date: 1977 },
-  { id: 7, name: 'Création de Facebook', creator: 'Mark Zuckerberg', collection: 'General', date: 2004 },
-  { id: 8, name: 'Invention de la carte à jouer', creator: '', collection: 'General', date: 900 },
-  { id: 9, name: 'Création de la NBA', creator: '', collection: 'General', date: 1946 },
-  { id: 10, name: 'Apparition du croissant', creator: '', collection: 'General', date: 1683 },
-  { id: 11, name: 'Invention de l’ampoule électrique', creator: 'Thomas Edison', collection: 'Invention', date: 1879 },
-  { id: 12, name: 'Invention du cinématographe', creator: 'Auguste et Louis Lumière', collection: 'Invention', date: 1895 },
-  { id: 13, name: 'Théorie de la relativité', creator: 'Albert Einstein', collection: 'Invention', date: 1915 },
-  { id: 14, name: 'Premier ordinateur portable', creator: 'Adam Osborne', collection: 'Invention', date: 1981 },
-  { id: 15, name: 'Apparition des abeilles', creator: '', collection: 'General', date: -100000000 },
-  { id: 16, name: 'Invention du dé', creator: '', collection: 'General', date: -3000 },
-  { id: 17, name: 'Début de la construction du phare d\'Alexandrie', creator: '', collection: 'General', date: -297 },
-  { id: 18, name: 'Cléopâtre est couronnée reine d\'Egypte', creator: '', collection: 'General', date: -51 },
-  { id: 19, name: 'Domestication du feu', creator: '', collection: 'Invention', date: -600000 },
-  { id: 20, name: 'Débuts de l\'écriture', creator: '', collection: 'Invention', date: -3400 },
-  { id: 21, name: 'Invention de la brosse à dents', creator: '', collection: 'Invention', date: 1498 },
-  { id: 22, name: 'Invention du microscope', creator: 'Zacharias Jansen', collection: 'Invention', date: 1590 },
-  { id: 23, name: 'Invention du thermomètre', creator: 'Galilée', collection: 'Invention', date: 1612 },
-  { id: 24, name: 'Invention du code morse', creator: 'Samuel Finley Morse', collection: 'Invention', date: 1838 },
+  { id: 0, name: 'Siège de Gergovie et d’Alésia', creator: '', collection: 'France', date: -52 },
+  { id: 1, name: 'Attila, le roi des Huns, envahit la Gaule', creator: '', collection: 'France', date: 451 },
+  { id: 2, name: 'Début du règne de Charlemagne', creator: '', collection: 'France', date: 768 },
+  { id: 3, name: 'Début de la construction de Notre-Dame de Paris', creator: '', collection: 'France', date: 1163 },            // OK
+  { id: 4, name: 'Premier recueil des Fables de La Fontaine', creator: '', collection: 'France', date: 1668 },                  // OK
+  { id: 5, name: 'Apparition des abeilles', creator: '', collection: 'General', date: -100000000 },                             // OK
+  { id: 6, name: 'Invention du dé', creator: '', collection: 'General', date: -3000 },                                          // OK
+  { id: 7, name: 'Début de la construction du phare d\'Alexandrie', creator: '', collection: 'General', date: -297 },           // OK
+  { id: 8, name: 'Cléopâtre est couronnée reine d\'Egypte', creator: '', collection: 'General', date: -51 },                    // OK
+  { id: 9, name: 'Invention de la carte à jouer', creator: '', collection: 'General', date: 900 },                              // OK
+  { id: 10, name: 'Apparition du croissant', creator: '', collection: 'General', date: 1683 },                                  // OK
+  { id: 11, name: 'Première apparition de Sherlock Holmes', creator: 'Arthur Conan Doyle', collection: 'General', date: 1887 }, // OK
+  { id: 12, name: 'Création de la NBA', creator: '', collection: 'General', date: 1946 },                                       // OK
+  { id: 13, name: 'Sortie du film La Guerre des Etoiles', creator: 'Georges Lucas', collection: 'General', date: 1977 },        // OK
+  { id: 14, name: 'Création de Facebook', creator: 'Mark Zuckerberg', collection: 'General', date: 2004 },                      // OK
+  { id: 15, name: 'Domestication du feu', creator: '', collection: 'Invention', date: -600000 },
+  { id: 16, name: 'Débuts de l\'écriture', creator: '', collection: 'Invention', date: -3400 },
+  { id: 17, name: 'Invention de la brosse à dents', creator: '', collection: 'Invention', date: 1498 },
+  { id: 18, name: 'Invention du microscope', creator: 'Zacharias Jansen', collection: 'Invention', date: 1590 },
+  { id: 19, name: 'Invention du thermomètre', creator: 'Galilée', collection: 'Invention', date: 1612 },
+  { id: 20, name: 'Invention du code morse', creator: 'Samuel Finley Morse', collection: 'Invention', date: 1838 },
+  { id: 21, name: 'Invention de l’ampoule électrique', creator: 'Thomas Edison', collection: 'Invention', date: 1879 },
+  { id: 22, name: 'Invention du cinématographe', creator: 'Auguste et Louis Lumière', collection: 'Invention', date: 1895 },
+  { id: 23, name: 'Théorie de la relativité', creator: 'Albert Einstein', collection: 'Invention', date: 1915 },
+  { id: 24, name: 'Premier ordinateur portable', creator: 'Adam Osborne', collection: 'Invention', date: 1981 },
 ];
-
-/*
-export  const cardsData = [
-  { id: 1, name: 'Le Parthénon', date: -447 },
-  { id: 2, name: 'Notre-Dame de Paris', date: 1162 },
-  { id: 3, name: 'Le mont Saint-Michel', date: 708 },
-  { id: 4, name: 'La pyramide de Khéops', date: -2560 },
-  { id: 5, name: 'Le Colisée', date: 70 },
-  { id: 6, name: 'La tour Eiffel', date: 1887 },
-  { id: 7, name: 'La tour de Pise', date: 1172 },
-  { id: 8, name: 'Le Palais idéal', date: 1879 },
-  { id: 9, name: 'La statue de la Liberté', date: 1886 },
-  { id: 10, name: 'Le Taj Mahal', date: 1621 },
-  { id: 11, name: 'Le cercle de Stonehenge', date: -2800 },
-  { id: 12, name: 'Les alignements de Carnac', date: -5000 }
-];
-
-  render(x, y, imgNb, additionalData) {
-    let card = this.scene.add.sprite(x, y, 'cards', imgNb).setScale(0.75);
-    card.cardData = additionalData;
-    return card;
-  }
-*/
